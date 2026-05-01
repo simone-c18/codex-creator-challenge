62ad1c5642178c7cd7f5abd586169faa30c035c6
simone-c18 <simone.chrastek5@gmail.com>
2026-04-30 22:03:33 -0400

    updated name in index.html

--- src/utils/parsePdf.js
+++ src/utils/parsePdf.js
@@ -1,11 +1,11 @@
 import * as pdfjsLib from "pdfjs-dist/legacy/build/pdf.mjs";
+import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
 export async function parsePdfToText(file) {
   const arrayBuffer = await file.arrayBuffer();
-  const pdf = await pdfjsLib.getDocument({
-    data: arrayBuffer,
-    disableWorker: true,
-  }).promise;
+  const pdf = await pdfjsLib.getDocument({ data: arrayBuffer }).promise;
   let fullText = "";
   for (let i = 1; i <= pdf.numPages; i++) {
     const page = await pdf.getPage(i);
